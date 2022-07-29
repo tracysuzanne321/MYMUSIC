6d676d8fa475f39032c1c5ed4b197f2d712a3c90
Tracy Smith <tracysuzanne321@gmail.com>
2022-07-29 16:40:53 +0100

    chore: initial navbar setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Settings from './pages/Settings';
+import Playlist from './pages/Signin';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 const App = () => {
-	return <div>Hello</div>;
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/settings" element={<Settings />} />
+					<Route path="/playlist" element={<Playlist />} />
+					<Route path="/signin" element={<Signin />} />
+					<Route path="/signup" element={<Signup />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,33 @@
+import { Link } from 'react-router-dom';
+import logo from '../images/logo.png';
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className="flex justify-between py-5 max-w-5xl mx-auto">
+			<div className="flex items-center space-x-5">
+				<Link className="flex items-center" to="/">
+					<img alt="logo" className="h-6 " src={logo} />
+					<div className=" text-xl">MyMusic</div>
+				</Link>
+				<Link to="/playlist">
+					<h3 className="text-white bg-pink-500 px-4 py-1 rounded-full cursor-pointer hover:bg-white hover:border-pink-500 hover:border hover:text-pink-500 hover:transition-all">
+						Playlist
+					</h3>
+				</Link>
+			</div>
+
+			<div className="flex items-center space-x-5 ">
+				<Link to="/signin">
+					<h3 className="cursor-pointer">Sign in</h3>
+				</Link>
+				<Link to="/signup">
+					<h3 className="border cursor-pointer py-1 px-4 rounded-full border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white">
+						Sign Up
+					</h3>
+				</Link>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
